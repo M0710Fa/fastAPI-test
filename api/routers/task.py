945a0347b03ae5e181841b94848b2bcf56c9eac3
M0710Fa <b1018188.fun@gmail.com>
2022-08-14 19:27:43 +0900

    add taskcreate fun

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,14 +1,18 @@
+from typing import List
+
 from fastapi import APIRouter
+
+import api.schemas.task as task_schema
 
 router = APIRouter()
 
 @router.get("/tasks")
 async def list_tasks():
-  pass
+  return [task_scheme.Task(id=1, title="１つ目のタスク")]
 
-@router.post("/tasks")
-async def create_task():
-  pass
+@router.post("/tasks", response_model = task_schema.TaskCreateResponse)
+async def create_task(task_body: task_schema.TaskCreate):
+  return task_schema.TaskCreateResponse(id= 1, **task_body.dict())
 
 @router.get("/tasks/{task_id}")
 async def update_task():
